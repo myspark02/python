9433170783823de4f17331714ed8a253adab4e10
myspark02 <myspark02@gmail.com>
2020-12-24 00:14:12 +0900

    partion modified for quick sort

--- .vscode/Sort/quick_sort.py
+++ .vscode/Sort/quick_sort.py
@@ -22,20 +22,44 @@ def partition(elements : list, start : int, end : int) -> int :
 
     return j
 
+def partition2(elements : list, start: int, end : int) -> int:
+    # print(f'start:{start}, end:{end}')
+    
+    pivot = elements[end]
+    i = start
+    j = end
+    
+    while i < j :
+        while elements[i] < pivot :
+            i += 1
+        
+        while elements[j] >= pivot :
+            j -= 1
+        
+        if i < j :
+            elements[i], elements[j] = elements[j], elements[i]
+
+    elements[end], elements[i] = elements[i], elements[end]
+
+    return i
+        
+
 def quick_sort(elements : list, start : int, end : int) -> None :
     if start >= end :
        return 
-    idx = partition(elements, start, end)
+    # idx = partition(elements, start, end)
+    idx = partition2(elements, start, end)
     quick_sort(elements, start, idx-1)
     quick_sort(elements, idx+1, end)
 
 
 if __name__ == '__main__' :
-    elements = []
+    # elements = []
 
-    for i in range(10) :
-        elements.append(random.randint(1, 100))
+    # for i in range(10) :
+    #     elements.append(random.randint(1, 100))
     
+    elements = [7, 6, 10, 5, 9, 2, 1, 15, 7]
     print(elements)
     quick_sort(elements, 0, len(elements)-1)
     print(elements)
